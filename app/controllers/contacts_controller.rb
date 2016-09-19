273346a2b2543ed68054b04dacee11c349f54051
Nasif Sufian <nasifsufean@gmail.com>
2016-09-19 18:14:37 +0000

    Added devise gem for users

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,8 +1,25 @@
 class ContactsController < ApplicationController
     def new
-     @contact=Contact.new
+         @contact = Contact.new
+    end
+                        
+    def create 
+        @contact = Contact.new(contact_params)
+        
+         if @contact.save
+             flash[:success] = "Message Sent :)"
+             redirect_to new_contact_path
+         else 
+             flash[:danger] = "Error. Message not sent!"
+             redirect_to new_contact_path
+         end
     end
     
-    def create 
-    end
+    private
+    
+        def contact_params
+            params.require(:contact).permit(:name, :email, :comments)
+        end
 end
+
+ 
