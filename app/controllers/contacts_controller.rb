ad337dc5dc921b16639681d546554d7b5251c9d3
Nasif Sufian <nasifsufean@gmail.com>
2016-09-20 15:20:23 +0000

    Added contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,5 +4,17 @@ class ContactsController < ApplicationController
     end
     
     def create 
+        @contact = Contact.new(contact_params)
+        if @contact.save
+            flash[:success] = 'Message Sent.'
+            redirect_to new_contact_path
+        else
+            flash[:danger] = 'error occurred, message could not be sent'
+            redirect_to new_contact_path
+            
+            private 
+            
+            def contact_params
+                params.require(:contact).permit(:name, :email, :comments)
     end
 end
