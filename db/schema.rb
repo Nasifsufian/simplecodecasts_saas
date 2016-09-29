2694df540607314bc72f312e8248c7867d643890
Nasif Sufian <nasifsufean@gmail.com>
2016-09-29 20:15:02 +0000

    Nothing much

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141223183721) do
+ActiveRecord::Schema.define(version: 20160920182958) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
@@ -22,10 +22,6 @@ ActiveRecord::Schema.define(version: 20141223183721) do
   end
 
   create_table "plans", force: true do |t|
-    t.string   "name"
-    t.decimal  "price"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
