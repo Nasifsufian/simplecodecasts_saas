be019a502ba459e9ea519f2ca60138e11b920e5e
Nasif Sufian <nasifsufean@gmail.com>
2016-10-04 16:08:27 +0000

    Modified devise for stripe

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,4 @@
 class Contact < ActiveRecord::Base
-
-     validates :name, presence: true
-      validates :email, presence: true
-
+  validates :name, presence: true
+  validates :email, presence: true
 end
