050c22f5c7134841146eafe1f74b3fa7a57fdfad
Nasif Sufian <nasifsufean@gmail.com>
2016-10-04 16:39:05 +0000

    Updated routes file for devise

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,8 +1,5 @@
+
 class Contact < ActiveRecord::Base
-<<<<<<< be019a502ba459e9ea519f2ca60138e11b920e5e
   validates :name, presence: true
   validates :email, presence: true
-=======
-    
->>>>>>> Added contacts form table and model
 end
