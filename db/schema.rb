3776f79c6d477a401613f474fd49cfa6794da86d
Nasif Sufian <nasifsufean@gmail.com>
2016-10-08 13:36:35 +0000

    Added profiles new action

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161007214014) do
+ActiveRecord::Schema.define(version: 20141224050503) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
